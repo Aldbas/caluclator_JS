f8ca1fa02321235831e30f79766e6073dc257633
AldBas <bascugaldwin@gmail.com>
2019-09-09 12:46:29 -0700

    operators functions

---
+++ calculator.js
@@ -0,0 +1,23 @@
+
+
+const add =  function addition(a, b) {
+	return a + b;
+}
+
+const subtract = function subtraction(a, b) {
+	return a - b;
+}
+
+const multiply = function multiplication(a ,b ){
+	return a * b;
+}
+
+const divide = function division(a,b) {
+		callback();
+
+	return a / b;
+}
+
+const operate = function operation(a, b, callback) {
+	callback(a,b);
+}
