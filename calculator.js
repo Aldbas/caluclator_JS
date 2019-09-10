cb21a8a39cbb76a1050beb4512b229b10c1b7b2c
AldBas <bascugaldwin@gmail.com>
2019-09-10 11:08:51 -0700

    added buttons numbers and operators

--- calculator.js
+++ calculator.js
@@ -1,23 +1,36 @@
 
-
-const add =  function addition(a, b) {
-	return a + b;
+let math = {
+	add: function(num1, num2){
+		return num1 + num2;
+	},
+	subtract: function(num1, num2) {
+		return num1 - num2;
+	},
+	multiply: function(num1, num2) {
+		return num1 * num2;
+	},
+	divide: function(num1, num2) {
+		return num1 / num2;
+	}
 }
 
-const subtract = function subtraction(a, b) {
-	return a - b;
-}
+// const add =  function addition(num1, num2) {
+// 	return num1 + num2;
+// }
 
-const multiply = function multiplication(a ,b ){
-	return a * b;
-}
+// const subtract = function subtraction(num1, num2) {
+// 	return num1 - num2;
+// }
 
-const divide = function division(a,b) {
-		callback();
+// const multiply = function multiplication(num1 ,num2 ){
+// 	return num1 * num2;
+// }
 
-	return a / b;
-}
+// const divide = function division(num1,num2{
 
-const operate = function operation(a, b, callback) {
-	callback(a,b);
+// 	return num1 / num2;
+// }
+
+const operate = function operation(math, num1, num2 ) {
+	return math(num1,num2);
 }
