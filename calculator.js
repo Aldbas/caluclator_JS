3378739f66d60f0f09239a2555d8e09e9d0d7236
AldBas <bascugaldwin@gmail.com>
2019-09-19 11:56:02 -0700

    finished

--- calculator.js
+++ calculator.js
@@ -45,25 +45,25 @@ function operation(operator, a, b) {
 
 
 buttons.forEach(button => button.addEventListener('click', function(e) {
+    let disabled = numBtns.forEach(button => button.disabled = false);
+
     let buttonClicked = e.target;
     if (display.textContent.length > 18) return;
 
     if (buttonClicked.value == '.' && display.textContent.includes('.')) return;
 
-    else if (buttonClicked.classList.contains('oprBtn')) {
+    if (buttonClicked.classList.contains('oprBtn')) {
         history.textContent = display.textContent + buttonClicked.value;
         num1 = parseInt(history.textContent.slice(0, -1));
         display.textContent = '';
         operator = buttonClicked.value;
-
 
     } else if (buttonClicked.classList.contains('equals')) {
 
         num2 = parseInt(display.textContent);
         results = operation(operator, num1, num2);
         if (results == 'Infinity') {
-            display.textContent = 'Error';
-            alert('who you fooling huh??');
+            display.textContent = 'Error :(';
         } else {
             display.textContent = results;
 
@@ -72,14 +72,9 @@ buttons.forEach(button => button.addEventListener('click', function(e) {
         numBtns.forEach(button => button.disabled = true);
     } else {
         display.textContent += buttonClicked.value;
-        numBtns.forEach(button => button.disabled = false);
-
     }
-console.log(e.keyCode);
 
 }));
-
-
 
 const equalBtn = document.getElementById('equals');
 const clearBtn = document.querySelector('.clear');
@@ -92,12 +87,26 @@ function clearInputs() {
     history.textContent = '';
 }
 
-function updateDisplay() {
-    display.textContent = '0';
-    history.textContent = '';
-
-}
-
 function back() {
     display.textContent = display.textContent.slice(0, -1);
 }
+//TODO: pressing down on keys
+// function keyPressed(e) {
+//     // const keyCodes = [8, 13, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98,
+//     //  99, 100, 101, 102, 103, 104, 105, 106, 107, 109, 110, 111, 187, 190, 173];
+//     const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+//     console.log(key);
+//     if(!key || display.textContent.length > 18) return; // do nothing if not part of keyCodes
+//     if(e.keyCode == 190 && display.textContent.includes('.')) return;
+//     else if(e.keyCode == 8 ){
+//    		back(); 	
+//     }
+//     else if (e.keyCode == 13) {
+
+//     }
+//      display.textContent += key.value; 	
+     
+    
+// }
+
+// window.addEventListener('keydown', keyPressed);
